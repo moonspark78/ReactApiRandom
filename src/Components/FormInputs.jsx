3bf3104f1a2465f5dc98a954f171270a8827960c
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-01-07 05:43:49 +0100

    finish inputs form  :tada:

--- src/Components/FormInputs.jsx
+++ src/Components/FormInputs.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./FormInputs.css"
 
 export const FormInputs = () => {
+    const [objData, setObjData] = useState({})
+    const getInputs = (name, value) => {
+        let data = {[name]: value}
+        setObjData({...objData, ...data});
+    };
+
+    const submit = () =>{
+        console.log(objData);
+    };
 
 
-    
+
 
 
 
   return (
     <div className='container'>
-        <input type='text' name='name' placeholder='Enter Your name'/>
+        <input type='text' name='name' placeholder='Enter Your name' onChange={(e) => getInputs(e.target.name, e.target.value)} />
         
-        <input type='number' name='age' placeholder='Enter your age'/>
+        <input type='number' name='age' placeholder='Enter your age' onChange={(e) => getInputs(e.target.name, e.target.value)}/>
         
-        <input type='text' name='email' placeholder='Enter your email'/>
+        <input type='text' name='email' placeholder='Enter your email' onChange={(e) => getInputs(e.target.name, e.target.value)}/>
         
-        <input type='date' name='date'/>
+        <input type='date' name='date' onChange={(e) => getInputs(e.target.name, e.target.value)}/>
         
-        <button type='submit'>Submit</button>
+        <button type='submit' onClick={submit}>Submit</button>
         
 
     </div>
