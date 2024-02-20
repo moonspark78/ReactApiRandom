00ed861e108d11ae795811936dc0df504730dffc
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-20 22:42:57 +0100

    test props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css';
 import { FormInputs } from './Components/FormInputs';
 import { Random } from './Components/Random';
+import { Callback } from './Components/Callback';
 
 function App() {
   return (
     <div className="App">
       {/* <Random/> */}
-      <FormInputs/>
+      {/* <FormInputs/> */}
+      <div className='coloo'>
+
+      </div>
+      
+      <Callback/>
     </div>
   );
 }
---
+++ src/Components/Callback.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+
+export const Callback = () => {
+
+
+
+  return (
+    <div>
+        <input  placeholder="Enter text here" class="input-style" type="text"/>
+    </div>
+  )
+}
