0e6b951fed42808bc7411e7334ca72bf11635833
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-02-20 23:37:20 +0100

    test props

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,24 @@ import './App.css';
 import { FormInputs } from './Components/FormInputs';
 import { Random } from './Components/Random';
 import { Callback } from './Components/Callback';
+import { useState } from 'react';
 
 function App() {
+  const [uiColor, setUiColor] = useState(null);
+
+  const handleColrChange = (color) =>{
+    setUiColor(color);
+  };
+
+
   return (
     <div className="App">
       {/* <Random/> */}
       {/* <FormInputs/> */}
-      <div className='coloo'>
-
+      <div className='coloo' style={{backgroundColor: uiColor}}>
       </div>
-      
-      <Callback/>
+
+      <Callback changeColor={handleColrChange}/>
     </div>
   );
 }
--- src/Components/Callback.jsx
+++ src/Components/Callback.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./style.css"
 
-export const Callback = () => {
+export const Callback = ({changeColor}) => {
+    const [search, setSearch] = useState("")
+
+    const handleInputChange = (e) =>{
+        const color = e.target.value;
+        setSearch(color);
+        changeColor(color);
+    };
 
 
 
   return (
     <div>
-        <input  placeholder="Enter text here" class="input-style" type="text"/>
+        <input  
+            placeholder="Enter text here" 
+            className="input-style" 
+            type="text"
+            value={search}
+            onChange={handleInputChange}
+        />
     </div>
   )
 }
